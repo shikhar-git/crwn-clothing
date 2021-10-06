54bde362278a16371b2a6a7bdefc1c71b51fbeb7
shikhar-git <59802844+shikhar-git@users.noreply.github.com>
2021-10-07 01:25:06 +0530

    corrected shop page abnormality

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -7,11 +7,13 @@ import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import "./collection-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => {
+  return(
   <div className="collections-overview">
-    {collections.map(({ id, ...otherCollection }) => (
-      <CollectionPreview key={id} {...otherCollection} />
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>;
+  </div>
+  )
 };
 
 const mapStateToProps = createStructuredSelector({
